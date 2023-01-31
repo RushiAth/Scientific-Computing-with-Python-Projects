5fba2652b36ab3b64c241d9276f06460f79df7c0
Rushi Athavale <69522459+RushiAth@users.noreply.github.com>
2023-01-31 00:06:09 -0500

    Update arithmetic_arranger.py

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -2,17 +2,18 @@ def arithmetic_arranger(problems, ansFlag=False):
 
   n = len(problems)
 
+  #checks error condition
   if n > 5:
     return "Error: Too many problems."
 
+  #creates lists and receives input
+  firstLine, secondLine, dashes, answers = [], [], [], []
+  
   splitProblems = [problems[i].split(" ") for i in range(n)]
 
-  firstLine = []
-  secondLine = []
-  dashes = []
-  answers = []
-
+  #loops over each problem
   for prob in splitProblems:
+    #checks problem specific error conditions
     if prob[1] != '+' and prob[1] != '-':
       return "Error: Operator must be '+' or '-'."
 
@@ -22,13 +23,15 @@ def arithmetic_arranger(problems, ansFlag=False):
     if len(prob[0]) > 4 or len(prob[2]) > 4:
       return "Error: Numbers cannot be more than four digits."
 
+    #determines formatting variables
     width = max(len(prob[0]), len(prob[2])) + 1
 
     if prob[1] == '+':
       temp = str(int(prob[0]) + int(prob[2]))
     else:
       temp = str(int(prob[0]) - int(prob[2]))
-
+  
+    #appends formatted string to each list    
     answers.append(' ' * (width - len(temp) + 1) + temp)
 
     dashes.append('-' * (width + 1))
@@ -36,6 +39,7 @@ def arithmetic_arranger(problems, ansFlag=False):
     firstLine.append(' ' * (width - len(prob[0]) + 1) + prob[0])
     secondLine.append(prob[1] + ' ' * (width - len(prob[2])) + prob[2])
 
+  #creates return string
   arranged_problems = '    '.join(firstLine) + '\n' + '    '.join(
     secondLine) + '\n' + '    '.join(dashes) 
   
